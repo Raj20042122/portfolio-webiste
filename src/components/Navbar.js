0bc6cfac5b30a995c97bcc9ec270073c6c9cbf11
RAJ <106920322+Raj20042122@users.noreply.github.com>
2026-03-18 10:42:40 +0530

    Remove unused imports from Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,9 @@ import Container from "react-bootstrap/Container";
 import logo from "../Assets/logo.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
 import { FaGithub } from "react-icons/fa";
 
 import {
-  AiFillStar,
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
